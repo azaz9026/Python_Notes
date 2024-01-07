e4eb10039fd47e76b1fa19ac97b13f6516d4602c
Md Azaz <mdazaz9026@gmail.com>
2024-01-07 22:11:01 +0530

    The global Keyword

--- Python Variables/Variables.py
+++ Python Variables/Variables.py
@@ -174,11 +174,49 @@ myfunc()
 
 
 # -------------------------------------------------------------------------------------------------------------
+
+'''
+If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. 
+The global variable with the same name will remain as it was, global and with the original value.
+'''
+
 x = "awesome"
 
 def myfunc():
   x = "fantastic"
   print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+# The global Keyword ---------------------------------------------------------------------------------------------
+
+'''
+Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+
+To create a global variable inside a function, you can use the global keyword.
+'''
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
 
 myfunc()
 
