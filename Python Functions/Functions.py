793b743e6789c4cdd01a93ddf7a35596287732fb
Md Azaz <mdazaz9026@gmail.com>
2024-01-10 22:19:25 +0530

    Recursion  function

--- Python Functions/Functions.py
+++ Python Functions/Functions.py
@@ -134,3 +134,140 @@ def my_function(**kid):
   print("His last name is " + kid["lname"])
 
 my_function(fname = "Md", lname = "Azaz")
+
+
+'''
+Default Parameter Value ----------------------------------------------------------------------------------------------------------------------------------------
+The following example shows how to use a default parameter value.
+
+If we call the function without argument, it uses the default value:
+'''
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+
+'''
+Passing a List as an Argument ----------------------------------------------------------------------------------------------------------------------------------
+You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
+
+E.g. if you send a List as an argument, it will still be a List when it reaches the function:
+'''
+def my_function(food):
+    for x in food:
+        print(x)
+
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+
+
+
+
+'''
+Return Values --------------------------------------------------------------------------------------------------------------------------------------------------
+To let a function return a value, use the return statement:
+'''
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+'''
+The pass Statement --------------------------------------------------------------------------------------------------------------------------------------------
+function definitions cannot be empty, but if you for some reason have a function 
+definition with no content, put in the pass statement to avoid getting an error.
+'''
+
+def myfunction():
+  pass
+
+
+
+
+'''
+Positional-Only Arguments -------------------------------------------------------------------------------------------------------------------------------------
+You can specify that a function can have ONLY positional arguments, or ONLY keyword arguments.
+
+To specify that a function can have only positional arguments, add , / after the arguments:
+'''
+
+
+def my_function(x, /):
+  print(x)
+
+my_function(3)
+
+# -----------------------------------------------
+
+def my_function(x):
+  print(x)
+
+my_function(x = 3)
+
+# -----------------------------------------------
+
+def my_function(x, /):
+  print(x)
+
+# my_function(x = 3)
+
+
+'''
+Keyword-Only Arguments -----------------------------------------------------------------------------------------------------------------------------------------
+To specify that a function can have only keyword arguments, add *, before the arguments:
+
+Example
+'''
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
+
+
+
+'''
+Combine Positional-Only and Keyword-Only -----------------------------------------------------------------------------------------------------------------------
+You can combine the two argument types in the same function.
+
+Any argument before the / , are positional-only, and any argument after the *, are keyword-only.
+
+Example
+'''
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
+
+
+'''
+Recursion------------------------------------------------------------------------------------------------------------------------------------------------------
+Python also accepts function recursion, which means a defined function can call itself.
+'''
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+
+
