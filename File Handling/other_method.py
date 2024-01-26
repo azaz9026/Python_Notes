a0d20ce5a3771b82eeba656234493588fa56bb77
Md Azaz <mdazaz9026@gmail.com>
2024-01-26 23:17:53 +0530

    truncate method

--- File Handling/other_method.py
+++ File Handling/other_method.py
@@ -12,3 +12,37 @@ with open("data.text" , 'r') as file:
     file.close()
 
 print(data)
+
+
+## tell method ------------------------------------------------------------------------------------------------
+
+with open("data.text" , 'r') as file:
+    data = file.read(5)
+    position = file.tell()
+    print( f' data :- {data} , Position :- {position} ' )
+
+
+## seek method ------------------------------------------------------------------------------------------------
+
+
+with open("data.text" , 'r') as file:
+    file.seek(3)
+    data = file.read(5)
+    print(data)
+
+
+## truncate method --------------------------------------------------------------------------------------------
+
+
+with open("data.text" , 'r+') as file:
+    file.truncate(5)
+    data = file.read()
+    print(data)
+
+
+## readable method -----------------------------------------------------------------------------------------------
+
+
+with open("data.text" , 'r+') as file:
+    print( f' this file is readable' , file.readable())
+   
